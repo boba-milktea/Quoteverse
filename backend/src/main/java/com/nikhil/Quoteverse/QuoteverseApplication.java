a8b08c816f493217a381c2b51d91419b3548686f
Nikhil Singhal <Nikhilsinghal476@gmail.com>
2025-10-06 16:46:05 +0530

    spring structure initialise

---
+++ backend/src/main/java/com/nikhil/Quoteverse/QuoteverseApplication.java
@@ -0,0 +1,13 @@
+package com.nikhil.Quoteverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteverseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nikhil/Quoteverse/QuoteverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.Quoteverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
