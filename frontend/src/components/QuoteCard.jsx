f2e91447175d50e05cbddba4aefb161fcb0b7eb6
boba-milktea <catherine.idv@gmail.com>
2025-10-07 23:34:23 +0200

    added all quotes page

--- frontend/src/components/QuoteCard.jsx
+++ frontend/src/components/QuoteCard.jsx
@@ -1,6 +1,12 @@
-
-const QuoteCard = () => {
-  return <div>Card</div>;
+const QuoteCard = ({ quote }) => {
+  const { text, author, tags } = quote;
+  return (
+    <div className="flex flex-col justify-center gap-4 p-4 text-center bg-surface border-border border-2 rounded-lg shadow-lg shadow-gray-600">
+      <h3 className="text-xl bold md:text-2xl"> &#10077; {text} &#10078;</h3>
+      <p className="font-bold text-xl"> - {author}</p>
+      <p># {tags.join(" #")}</p>
+    </div>
+  );
 };
 
 export default QuoteCard;
