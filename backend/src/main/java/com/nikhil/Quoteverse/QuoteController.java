e91da83d8a85c8b196e216e2a802ff3326162b04
charlcoalDev <44064994+charlcoalDev@users.noreply.github.com>
2025-10-07 19:16:06 +0300

    Implement GET /api/quotes endpoint to return all quotes from quotes.json

---
+++ backend/src/main/java/com/nikhil/Quoteverse/QuoteController.java
@@ -0,0 +1,51 @@
+package com.nikhil.Quoteverse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class QuoteController {
+
+    @GetMapping("/quotes")
+    public ResponseEntity<?> getAllQuotes() {
+        try {
+        
+            String basePath = System.getProperty("user.dir"); // should be ...\Quoteverse\Quoteverse\backend
+            Path filePath = Path.of(basePath)
+                .resolve("QuoteVerse")  
+                .resolve("data")     // enter data folder
+                .resolve("quotes.json")
+                .normalize();
+
+            File file = filePath.toFile();
+
+
+            if (!file.exists()) {
+                return ResponseEntity.status(404)
+                        .body(Map.of("error", "quotes.json not found at " + file.getAbsolutePath()));
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+            List<Map<String, Object>> quotes = mapper.readValue(file, new TypeReference<List<Map<String, Object>>>() {});
+
+
+            if (quotes.isEmpty()) {
+                return ResponseEntity.status(404).body(Map.of("error", "no quotes found"));
+            }
+
+            return ResponseEntity.ok(quotes);
+
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body(Map.of("error", e.getMessage()));
+        }
+    }
+}
